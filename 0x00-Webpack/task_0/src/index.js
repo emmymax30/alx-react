c1e0c7187f5454984a91c414d1cf7cb90af8297a
emmymax30 <Holabodeemmanuel30@gmail.com>
2023-02-20 21:35:33 +0000

    webpack

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$(document).ready(function() {
+	  $('body').append('<p>This is the first paragraph.</p>');
+	  $('body').append('<p>This is the second paragraph.</p>');
+	  $('body').append('<p>This is the third paragraph.</p>');
+});
